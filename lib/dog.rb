af1f365472594de21df72ef34224a729a5890de7
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 22:03:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
   attr_accessor :name, :breed
 
-  def initialize(id: = nil, name:, breed:)
+  def initialize(id = nil, name, breed)
     @id = id
     @name = name
     @breed = breed
   end
 
-  def 
+  def
 end
