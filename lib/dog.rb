414cc81d1117f7c27ecd5f1b5fc1e53d27c03de1
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 22:53:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,8 @@ class Dog
     end
   end
 
-  def self.create(name, breed)
-    dog = Dog.new(name: name, breed: breed)
+  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
     dog.save
     dog
   end
