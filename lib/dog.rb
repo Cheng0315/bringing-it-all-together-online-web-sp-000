e372e0cc8fa4d5c700305c2484456a02d5a0389f
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 22:21:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,7 +35,7 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
 
-      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")
+      self[:id] = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")
     end
   end
 
