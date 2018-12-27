de9adb530cdfadfbc6efdada4f7916583cebdb5e
Cheng Vue <cvu3@ucsc.edu>
2018-12-27 00:02:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
     if dog.empty?
       self.create(hash)
     elsif !dog.empty?
-      @@all.find {|d| d.name == hash[:name]}
+      @@all.find {|d| d.id == hash[:id]}
     end
   end
 end
